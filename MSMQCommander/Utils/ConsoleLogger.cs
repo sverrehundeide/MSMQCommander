1c3f4616663fe442f53a41bb3ab75b0c82266f58
Sverre Hundeide <github@hundeide.net>
2011-07-02 15:18:28 +0200

    Added ConsoleLogger. Added initial version of databound treeview. Use trunk version of Caliburn.Micro.

---
+++ MSMQCommander/Utils/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace MSMQCommander.Utils
+{
+    public class ConsoleLogger : ILog
+    {
+        public void Info(string format, params object[] args)
+        {
+            Console.WriteLine(string.Format(format, args));
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            Console.WriteLine(string.Format(format, args));
+        }
+
+        public void Error(Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+        }
+    }
+}
